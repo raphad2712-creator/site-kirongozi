f82e615d218335678dcb77065d5ea6622fd698e8
Raphael David Silva Freitas <raphad2712@gmail.com>
2026-09-16 08:03:34 -0300

    Abre a página instantaneamente no topo

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function openAtStart() {
   if (window.location.hash) {
     history.replaceState(null, '', `${window.location.pathname}${window.location.search}`);
   }
-  window.scrollTo(0, 0);
+  window.scrollTo({ top: 0, left: 0, behavior: 'instant' });
 }
 
 function resetEntryPosition() {
